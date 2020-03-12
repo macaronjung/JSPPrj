4878b3b5b304a2f4584a77db6012299d3497c8ff
macaronjung <macaronjung@gmail.com>
2020-03-13 00:21:33 +0900

    service 메소드 사용 이유 주석 추가

--- src/com/newlecture/web/Add.java
+++ src/com/newlecture/web/Add.java
@@ -10,6 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({ "/Add", "/add" })
 public class Add extends HttpServlet {
 
+	//왜 service 메소드를 사용하는지 인터넷 검색결과
+	//서블릿 생명주기 때문에 사용!!!
+	/* 1.요청이 오면, Servlet 클래스가 로딩되어 요청에 대한 Servlet 객체가 생성
+	 * 2.서버는 init() 메소드를 호출해서 Servlet을  초기화
+	 * 3.service() 메소드를 호출해서 Servlet이 브라우저의 요청을 처리
+	 * 4.service() 메소드는 특정 HTTP 요청(GET, POST 등)을 처리하는 메서드 (doGet(), doPost() 등)를 호출
+	 * 5.서버는 destroy() 메소드를 호출하여 Servlet을 제거
+	 * https://victorydntmd.tistory.com/154
+	 * */
+	/* 서블릿 의 service () 메소드는 클라이언트 요청에 대해 서블릿에 알리기 위해 호출
+	 * ServletRequest 오브젝트를 사용 하여 클라이언트가 요청한 데이터를 수집
+	 * 이 메소드는 ServletResponse 오브젝트를 사용하여 출력 컨텐츠를 생성 
+	 * geeksforgeeks.org/life-cycle-of-a-servlet/
+	 * */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//22강 과제풀이
 			//인코딩 변환
